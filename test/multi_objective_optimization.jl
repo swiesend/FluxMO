72b553543c6fb16bb6e9f858ed7e112e8b1222d7
swiesend <sebastian@wiesendahl.de>
2018-05-15 13:32:56 -0500

    update with StackOverflow example

--- test/multi_objective_optimization.jl
+++ test/multi_objective_optimization.jl
@@ -12,9 +12,9 @@ using FluxMO.Validation: betacv
 function train(seed::Int)
     srand(seed)
 
-    N = 10000   # samples
+    N = 1000    # samples
     M = 100     # data size
-    L = 2       # latent space
+    L = 2       # latent space size
 
     D = map(_->rand(M),1:N)
     X = deepcopy(D[1:end-1])
@@ -29,11 +29,12 @@ function train(seed::Int)
         Dense(10,  100, a),
         Dense(100, M,   sigmoid),
     )
-    el = 3
+    embd_layer = 3
 
     function supervised_betacv()
+        m_copy = deepcopy(m)
         # Embed all samples from X into the latent space of size L
-        embds_tracked = map(x-> m[1:el](x), X)
+        embds_tracked = map(x-> m_copy[1:embd_layer](x), X)
         embds  = hcat(map(ta->ta.data, embds_tracked)...)
         
         # Cluster the current embeddings without tracking to generate
@@ -42,17 +43,18 @@ function train(seed::Int)
         clustering, _ = knn_clustering(embds)
 
         # Obtain tracked embedded values from the clustering
-        values_tracked = map(c->map(i->embds_tracked[i],c), clustering)
-
+        embds_clustered_tracked = map(c->map(i->embds_tracked[i],c), clustering)
         # Validate the clustering via BetaCV measure (small is good).
         # This is done with the tracked values, as it should influence
         # the model weights and biases towards optimizing this measure.
-        bcv = betacv(values_tracked)
+        bcv = betacv(embds_clustered_tracked)
+        @show bcv
 
         bcv
     end
 
-    function loss(x, y)
+    function loss(x, y, i)
+
         # unsupervised
         ŷ = m(x)
         ce = crossentropy(ŷ, y)
@@ -65,9 +67,8 @@ function train(seed::Int)
         # How to backtrack this function efficently?
         #   * Do I have to treat it as second model with an supervised setting?
         #   * or can I apply the `betacv` as intendet to the existing model?
-        # TODO:
-        # apply after a certain amount of training
-        if false 
+        if i != -1 && i % floor(Int, (N-1)/3) == 0
+            println("applying betacv after $i samples")
             bcv = supervised_betacv()
         end
 
@@ -79,18 +80,19 @@ function train(seed::Int)
 
     function callback()
         ns = rand(1:N-1, round(Int, sqrt(N)))
-        ls = map(n->loss(X[n], Y[n]).tracker.data, ns)
+        ls = map(n->loss(X[n], Y[n], -1).tracker.data, ns)
         println("Training:  loss: ", sum(ls), "\tstd: ", std(ls))
 
         # embeddings
-        es = m[1:el](X[rand(1:N-1)]).data
+        es = m[1:embd_layer](X[rand(1:N-1)]).data
         println("Embedding: ", es)
+        
         println()
     end
 
     for epoch in 1:1
         info("Epoch: $epoch (Seed: $seed)")
-        Flux.train!(loss, zip(X, Y), opt, cb=throttle(callback,3))
+        Flux.train!(loss, zip(X, Y, collect(1:length(X))), opt, cb=throttle(callback,3))
     end
 end
 
